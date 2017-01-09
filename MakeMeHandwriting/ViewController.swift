89ccc8d9c2a586883c994ffe9f9a0dfd00b4c0b0
Axel Hugon <marat@macbook-pro-de-axel.local>
2017-01-09 17:04:02 +0100

    Hide/Unhide Keyboard first view

--- MakeMeHandwriting/ViewController.swift
+++ MakeMeHandwriting/ViewController.swift
@@ -16,17 +16,49 @@ class ViewController: UIViewController {
     @IBOutlet var containerView: UIView!
     @IBOutlet var containerScrollView: UIScrollView!
     
+    //Constraints
+    @IBOutlet var buttonBottomConstraint: NSLayoutConstraint!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         
     }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.buttonBottomConstraint.constant == 0{
+                self.buttonBottomConstraint.constant += keyboardSize.height
+            }
+        }
+        
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.buttonBottomConstraint.constant != 0{
+                self.buttonBottomConstraint.constant -= keyboardSize.height
+            }
+        }
+    }
+    
 
+    func keyboardWasShown(notification: NSNotification) {
+        let info = notification.userInfo!
+        let keyboardFrame: CGRect = (info[UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        
+        UIView.animate(withDuration: 0.1, animations: { () -> Void in
+            self.buttonBottomConstraint.constant = keyboardFrame.size.height + 20
+        })
+    }
 
 }
 
