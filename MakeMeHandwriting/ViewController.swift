13ea5fbd0a90c2952c6dc173d9ffcf5031c548b8
Axel Hugon <marat@macbook-pro-de-axel.local>
2017-01-09 09:18:13 +0100

    Initial Commit

---
+++ MakeMeHandwriting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MakeMeHandwriting
+//
+//  Created by Axel Hugon on 09/01/2017.
+//  Copyright © 2017 AHUGON. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
