024965c04d2f86212f67e6e4998f7edae7202151
Axel Hugon <marat@macbook-pro-de-axel.local>
2017-01-10 23:55:39 +0100

    Share Button, closes #5, closes #6, closes #7, closes #8 and more

--- MakeMeHandwriting/HandwritingResultViewController.swift
+++ MakeMeHandwriting/HandwritingResultViewController.swift
@@ -43,6 +43,22 @@ class HandwritingResultViewController: UIViewController {
         
     }
     
+    @IBAction func onShareButtonTouchUpInside(_ sender: Any) {
+        
+        // image to share
+        let image = handwritingPngImage
+        
+        // set up activity view controller
+        let imageToShare = [ image ]
+        let activityViewController = UIActivityViewController(activityItems: imageToShare, applicationActivities: nil)
+        activityViewController.popoverPresentationController?.sourceView = self.view // so that iPads won't crash
+        
+        // exclude some activity types from the list (optional)
+        //activityViewController.excludedActivityTypes = [ UIActivityType.airDrop, UIActivityType.postToFacebook ]
+        
+        // present the view controller
+        self.present(activityViewController, animated: true, completion: nil)
+    }
     
     
 
